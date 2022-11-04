b07a5456268730ac29bf517c5860c02dd484140d
lasteeh <lastdavid.lantican@gmail.com>
2022-11-05 05:31:26 +0800

    add deposit, withdraw, transfer functions in progress

--- src/Components/Transfer.js
+++ src/Components/Transfer.js
@@ -1,6 +1,73 @@
 import React from "react";
 import Transaction from "./SubComponents/Transaction";
+import { useAuth } from "../Auth";
+import {
+  disableButton,
+  enableButton,
+} from "./SubComponents/InterfaceFunctions";
 
 export default function Transfer() {
-  return <Transaction transactionType="transfer" />;
+  const auth = useAuth();
+  const userData = auth.user;
+
+  /* set deposit (not confirmed state) */
+  const [userBalance, setUserBalance] = React.useState(userData.balance);
+  /* confirm deposit (submission state) */
+  const [userCurrentData, setUserCurrentData] = React.useState(userData);
+  /* set review deposit amount */
+  const [reviewInputAmount, setReviewInputAmount] = React.useState(0);
+  /* set state of submitConfirm button */
+  const [isDisabled, setIsDisabled] = React.useState(true);
+
+  const input = React.useRef(null);
+
+  function onClickSubmitEvent() {
+    setIsDisabled(true);
+    disableButton("submitConfirm");
+
+    input.current.focus();
+  }
+
+  function enterTransfer() {
+    const inputValue = parseInt(document.querySelector(`#transfer`).value);
+    if (!inputValue) {
+      setIsDisabled(true);
+      disableButton("submitConfirm");
+      setReviewInputAmount(0);
+      console.log("enter withdraw amount");
+    } else if (inputValue > userData.balance) {
+      setIsDisabled(true);
+      disableButton("submitConfirm");
+      console.log("negatib pre");
+    } else {
+      setIsDisabled(false);
+      enableButton("submitConfirm");
+      setReviewInputAmount(inputValue);
+      setUserBalance(parseInt(userData.balance) - inputValue);
+      console.log(userBalance);
+      return inputValue;
+    }
+  }
+
+  function confirmTransfer() {
+    /* setUserCurrentData((prevUserData) => {
+      return { ...prevUserData, balance: 10000000 };
+    }); */
+    console.log(userCurrentData);
+  }
+
+  return (
+    <Transaction
+      transactionType="transfer"
+      currentBalance={userBalance}
+      transactionProcess={enterTransfer}
+      submitTransaction={confirmTransfer}
+      inputValue={reviewInputAmount}
+      userName={userData.name}
+      userEmail={userData.email}
+      isSubmitTransactionButtonDisabled={isDisabled}
+      onClickSubmitEvent={onClickSubmitEvent}
+      input={input}
+    />
+  );
 }
--- src/Components/Withdraw.js
+++ src/Components/Withdraw.js
@@ -1,6 +1,73 @@
 import React from "react";
 import Transaction from "./SubComponents/Transaction";
+import { useAuth } from "../Auth";
+import {
+  disableButton,
+  enableButton,
+} from "./SubComponents/InterfaceFunctions";
 
 export default function Withdraw() {
-  return <Transaction transactionType="withdraw" />;
+  const auth = useAuth();
+  const userData = auth.user;
+
+  /* set deposit (not confirmed state) */
+  const [userBalance, setUserBalance] = React.useState(userData.balance);
+  /* confirm deposit (submission state) */
+  const [userCurrentData, setUserCurrentData] = React.useState(userData);
+  /* set review deposit amount */
+  const [reviewInputAmount, setReviewInputAmount] = React.useState(0);
+  /* set state of submitConfirm button */
+  const [isDisabled, setIsDisabled] = React.useState(true);
+
+  const input = React.useRef(null);
+
+  function onClickSubmitEvent() {
+    setIsDisabled(true);
+    disableButton("submitConfirm");
+
+    input.current.focus();
+  }
+
+  function enterWithdraw() {
+    const inputValue = parseInt(document.querySelector(`#withdraw`).value);
+    if (!inputValue) {
+      setIsDisabled(true);
+      disableButton("submitConfirm");
+      setReviewInputAmount(0);
+      console.log("enter withdraw amount");
+    } else if (inputValue > userData.balance) {
+      setIsDisabled(true);
+      disableButton("submitConfirm");
+      console.log("negatib pre");
+    } else {
+      setIsDisabled(false);
+      enableButton("submitConfirm");
+      setReviewInputAmount(inputValue);
+      setUserBalance(parseInt(userData.balance) - inputValue);
+      console.log(userBalance);
+      return inputValue;
+    }
+  }
+
+  function confirmWithdraw() {
+    /* setUserCurrentData((prevUserData) => {
+      return { ...prevUserData, balance: 10000000 };
+    }); */
+    console.log(userCurrentData);
+  }
+
+  return (
+    <Transaction
+      transactionType="withdraw"
+      currentBalance={userBalance}
+      transactionProcess={enterWithdraw}
+      submitTransaction={confirmWithdraw}
+      inputValue={reviewInputAmount}
+      userName={userData.name}
+      userEmail={userData.email}
+      isSubmitTransactionButtonDisabled={isDisabled}
+      onClickSubmitEvent={onClickSubmitEvent}
+      input={input}
+    />
+  );
 }
