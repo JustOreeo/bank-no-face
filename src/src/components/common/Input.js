2258f96bc2ef2e9cb02e6e8826a4052e9ce5e0f1
JustOreeo <52491896+JustOreeo@users.noreply.github.com>
2022-11-05 12:07:48 +0800

    patch: components

---
+++ src/src/components/common/Input.js
@@ -0,0 +1,32 @@
+const fixedInputClass="rounded-md appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm"
+
+const Input = ({
+    handleChange,
+    value,
+    labelText,
+    labelFor,
+    id,
+    name,
+    type,
+    isRequired=false,
+    placeholder,
+}) => {
+    return(
+      <div className="my-5">
+        <label htmlFor={labelFor} className="sr-only">
+          {labelText}
+        </label>
+        <input
+          onChange={handleChange}
+          value={value}
+          id={id}
+          name={name}
+          type={type}
+          className={fixedInputClass}
+          placeholder={placeholder}
+        />
+        </div>
+    )
+}
+
+export default Input;
