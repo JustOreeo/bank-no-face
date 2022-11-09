a1171ea4fe06f8bd261550ec8487c313ef902b06
lasteeh <lastdavid.lantican@gmail.com>
2022-11-10 07:31:39 +0800

    style: started styling account details and main header

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import AccountDetails from '../components/common/AccountDetails'
-import RecentTransactions from '../components/common/RecentTransactions'
+import React from "react";
+import AccountDetails from "../components/common/AccountDetails";
+import RecentTransactions from "../components/common/RecentTransactions";
 
-const Admin = ({user}) => {
-    console.log("Admin: ",user)
-    console.log("Admin Name: ",user.name)
-    console.log("Admin Name: ",user.name)
+const Admin = ({ user }) => {
+  console.log("Admin: ", user);
+  console.log("Admin Name: ", user.name);
+  console.log("Admin Name: ", user.name);
   return (
-    <div>
-        Admin
-        <AccountDetails userInfo={user}/>
-        <RecentTransactions userInfo={user}/>
+    <div className="admin-page">
+      <h2>Admin</h2>
+      <AccountDetails userInfo={user} />
+      <RecentTransactions userInfo={user} />
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
