8799d1df4c2cee8bb040202a4efd40547eb31449
Michelle Santiago <mdcsantiago0@gmail.com>
2022-11-10 13:20:43 +0800

    fix merge conflict

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Dashboard from './components/Dashboard';
 import ProtectedRoutes from './components/services/ProtectedRoutes';
 import CreateAccount from './pages/CreateAccount';
 import Users from './pages/Users';
-import Withdraw from './pages/Withdraw';
-import Transfer from './pages/Transfer';
+import Transfers from './pages/Transfers';
+import Manage from './pages/Manage';
 import Transaction from './pages/Transaction';
 const App = () => {
     return (
