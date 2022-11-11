0f5cedeb688c048f00d189b13b2992cfe93f9ea4
lasteeh <lastdavid.lantican@gmail.com>
2022-11-11 19:10:54 +0800

    style: fix new uupdates

--- src/components/common/Transactions.js
+++ src/components/common/Transactions.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import { demoRecentTransactions } from "../../constants/demoRecentTransactions";
 
 const Transactions = ({ userInfo, showTransaction }) => {
@@ -6,7 +6,7 @@ const Transactions = ({ userInfo, showTransaction }) => {
   console.log("Recent Transactions User Role: ", userInfo.role);
   console.log("Recent Transactions User Email: ", userInfo.email);
   console.log("Show Transaction: ", showTransaction);
-  const [empty,setEmpty]=useState('');
+  const [empty, setEmpty] = useState("");
 
   const recentTransactions = demoRecentTransactions;
   let userTransactions = [];
@@ -37,68 +37,66 @@ const Transactions = ({ userInfo, showTransaction }) => {
     }
   });
   useEffect(() => {
-    if(userTransactions.length===0){
-        setEmpty("true")
-    }else{
-        setEmpty("false")
+    if (userTransactions.length === 0) {
+      setEmpty("true");
+    } else {
+      setEmpty("false");
     }
-  })
+  });
 
-let headers;
-let transactionsInfo;
-if(userTransactions.length!==0){
+  let headers;
+  let transactionsInfo;
+  if (userTransactions.length !== 0) {
     //reverse to get the latest transaction on top
     userTransactions.reverse();
     //check user transactions
-    console.log("Transactions Current User: ",userTransactions)
+    console.log("Transactions Current User: ", userTransactions);
     //headers for table
-    headers=Object.keys(userTransactions[0]);
+    headers = Object.keys(userTransactions[0]);
     //rows value
-    transactionsInfo=Object.values;
-}
-return (
+    transactionsInfo = Object.values;
+  }
+  return (
     <div className="transactions">
       <div className="flex gap-4 justify-content items-center">
         <span className="component-header">
           {showTransaction !== "All" && "Recent "}Transactions
         </span>
-        {showTransaction !== "All" && empty==="false" &&  (
-          <button className="btn btn-xs">View More</button>
+        {showTransaction !== "All" && empty === "false" && (
+          <button className="green-button view-more">View More</button>
         )}
       </div>
-        <div className="overflow-x-auto">
-            <table className="table table-compact text-center">
-                {empty===false&&
-                    <thead>
-                        
-                        <tr>
-                            <th></th>
-                            {headers.map((key) => (
-                            <th>{key}</th>
-                            ))}
-                        </tr>
-                        
-                    </thead>
-                }
-                <tbody>
-                {userTransactions.map((item,index) => (
-                    <tr key={index} className="hover">
-                        <th>{index+1}</th>
-                        {transactionsInfo(item).map((value) => (
-                            <td>{value}</td>
-                        ))}
-                    </tr>
+      <div className="overflow-x-auto">
+        <table className="table table-compact text-center">
+          {empty === false && (
+            <thead>
+              <tr>
+                <th></th>
+                {headers.map((key) => (
+                  <th>{key}</th>
                 ))}
-                {empty==="true"&&
-                    <tr>
-                        <td>Nothing to show here</td>
-                    </tr>
-                }
-                </tbody>
-            </table>
-        </div>
+              </tr>
+            </thead>
+          )}
+          <tbody>
+            {userTransactions.map((item, index) => (
+              <tr key={index} className="hover">
+                <th>{index + 1}</th>
+                {transactionsInfo(item).map((value) => (
+                  <td>{value}</td>
+                ))}
+              </tr>
+            ))}
+            {empty === "true" && (
+              <tr>
+                <td>Nothing to show here</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
-)
-}
+  );
+};
 
-export default Transactions
+export default Transactions;
