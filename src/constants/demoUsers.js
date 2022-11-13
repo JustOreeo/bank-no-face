2bd6aa2ba9d12882bbeff912ecd7aaaabf789e37
lasteeh <lastdavid.lantican@gmail.com>
2022-11-13 17:16:43 +0800

    feat: dashboard user count chart now functional and showing total users created per day

--- src/constants/demoUsers.js
+++ src/constants/demoUsers.js
@@ -1,76 +1,85 @@
 const demoUsers = [
-    {
-        name: 'Lee Ji-eun',
-        email: 'iu@email.com',
-        value: 'iu@email.com',
-        password: '12345678',
-        role: 'user',
-        balance: 5214415
-    },
-    {
-        name: 'Gong Yoo',
-        email: 'gy@email.com',
-        value: 'gy@email.com',
-        password: '12345678',
-        role: 'user',
-        balance: 521899
-    },
-    {
-        name: 'Lee Min-ho',
-        email: 'lmh@email.com',
-        value: 'lmh@email.com',
-        password: '12345678',
-        role: 'user',
-        balance: 5215151
-    },
-    {
-        name: 'Park Seo-joon',
-        email: 'psh@email.com',
-        value: 'psh@email.com',
-        password: '12345678',
-        role: 'user',
-        balance: 125151
-    },
-    {
-        name: 'Jun Ji Hyun',
-        email: 'jjh@email.com',
-        value: 'jjh@email.com',
-        password: '125215',
-        role: 'user',
-        balance: 215151
-    },
-    {
-        name: 'Seo In-guk',
-        email: 'sig@email.com',
-        value: 'sig@email.com',
-        password: '1151521',
-        role: 'user',
-        balance: 2515120
-    },
-    {
-        name: 'Kim Soo-Hyun',
-        email: 'ksh@email.com',
-        value: 'ksh@email.com',
-        password: '12345678',
-        role: 'user',
-        balance: 100000
-    },
-    {
-        name: 'Park Shin-hye',
-        email: 'psh@email.com',
-        value: 'psh@email.com',
-        password: '12345678',
-        role: 'user',
-        balance: 100000
-    },
-    {
-        name: 'Song Hye-kyo',
-        email: 'shk@email.com',
-        value: 'shk@email.com',
-        password: '12345678',
-        role: 'user',
-        balance: 1000000
-    }
-]
+  {
+    name: "Lee Ji-eun",
+    email: "iu@email.com",
+    value: "iu@email.com",
+    password: "12345678",
+    role: "user",
+    balance: 5214415,
+    dateCreated: "11/01/2022" /* new Date().toLocaleDateString() */,
+  },
+  {
+    name: "Gong Yoo",
+    email: "gy@email.com",
+    value: "gy@email.com",
+    password: "12345678",
+    role: "user",
+    balance: 521899,
+    dateCreated: "11/01/2022",
+  },
+  {
+    name: "Lee Min-ho",
+    email: "lmh@email.com",
+    value: "lmh@email.com",
+    password: "12345678",
+    role: "user",
+    balance: 5215151,
+    dateCreated: "11/02/2022",
+  },
+  {
+    name: "Park Seo-joon",
+    email: "psh@email.com",
+    value: "psh@email.com",
+    password: "12345678",
+    role: "user",
+    balance: 125151,
+    dateCreated: "11/02/2022",
+  },
+  {
+    name: "Jun Ji Hyun",
+    email: "jjh@email.com",
+    value: "jjh@email.com",
+    password: "125215",
+    role: "user",
+    balance: 215151,
+    dateCreated: "11/02/2022",
+  },
+  {
+    name: "Seo In-guk",
+    email: "sig@email.com",
+    value: "sig@email.com",
+    password: "1151521",
+    role: "user",
+    balance: 2515120,
+    dateCreated: "11/03/2022",
+  },
+  {
+    name: "Kim Soo-Hyun",
+    email: "ksh@email.com",
+    value: "ksh@email.com",
+    password: "12345678",
+    role: "user",
+    balance: 100000,
+    dateCreated: "11/04/2022",
+  },
+  {
+    name: "Park Shin-hye",
+    email: "psh@email.com",
+    value: "psh@email.com",
+    password: "12345678",
+    role: "user",
+    balance: 100000,
+    dateCreated: "11/04/2022",
+  },
+  {
+    name: "Song Hye-kyo",
+    email: "shk@email.com",
+    value: "shk@email.com",
+    password: "12345678",
+    role: "user",
+    balance: 1002000,
+    dateCreated: "11/06/2022",
+  },
+];
 
 export { demoUsers };
