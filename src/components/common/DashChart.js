e63a1524b91f6708b2f822ee7b1b55d0c6d8ef04
lasteeh <lastdavid.lantican@gmail.com>
2022-11-13 13:38:38 +0800

    feat: dashchart functional with demo data

--- src/components/common/DashChart.js
+++ src/components/common/DashChart.js
@@ -1,24 +1,41 @@
 import React, { Component } from "react";
 import Chart from "react-apexcharts";
+import { demoChartData } from "../../constants/demoChartData";
 
 class DashChart extends Component {
   constructor(props) {
     super(props);
+    const yTotalUsers = demoChartData.map((chartdata) => chartdata.users);
+    const yTotalMoney = demoChartData.map((chartdata) => chartdata.money);
+    const xDate = demoChartData.map((chartdata) => {
+      return `${chartdata.date} GMT`;
+    });
+
+    console.table(
+      "12345678945612345678945612315648123",
+      yTotalUsers,
+      yTotalMoney,
+      xDate
+    );
+
     this.state = {
       series: [
         {
           name: "Total Number of Users",
           type: "line",
-          data: [1, 2, 3],
+          data: [...yTotalUsers],
         },
         {
           name: "Money in the Bulsa",
           type: "column",
-          data: [10000, 20000, 30000],
+          data: [...yTotalMoney],
         },
       ],
       options: {
         chart: {
+          toolbar: {
+            show: false,
+          },
           height: "550",
           type: "line",
           fontFamily: "inherit",
@@ -41,21 +58,36 @@ class DashChart extends Component {
         dataLabels: {
           enabled: false,
         },
-        labels: ["01 Nov 2022", "02 Nov 2022", "03 Nov 2022"],
+        labels: [...xDate],
         xaxis: {
-          type: "datetime",
+          range: 4,
         },
         yaxis: [
           {
             /* title: {
-              text: "Money in the Bulsa",
-            }, */
-          },
-          {
-            opposite: true,
-            /* title: {
               text: "Total Number of Users1",
             }, */
+            labels: {
+              show: true,
+              formatter: (value) => {
+                return value.toFixed(0);
+              },
+            },
+          },
+          {
+            /* title: {
+                text: "Money in the Bulsa",
+              }, */
+            opposite: true,
+            labels: {
+              show: true,
+              formatter: (value) => {
+                return new Intl.NumberFormat("fil-PH", {
+                  style: "currency",
+                  currency: "PHP",
+                }).format(value);
+              },
+            },
           },
         ],
       },
---
+++ src/constants/demoChartData.js
@@ -0,0 +1,49 @@
+const demoChartData = [
+  {
+    users: 1,
+    date: "11/01/2022",
+    money: 1000,
+  },
+  {
+    users: 3,
+    date: "11/02/2022",
+    money: 2000,
+  },
+  {
+    users: 4,
+    date: "11/03/2022",
+    money: 127700,
+  },
+  {
+    users: 6,
+    date: "11/04/2022",
+    money: 130000,
+  },
+  {
+    users: 9,
+    date: "11/05/2022",
+    money: 500000,
+  },
+  {
+    users: 10,
+    date: "11/06/2022",
+    money: 18820000,
+  },
+  {
+    users: 11,
+    date: "11/07/2022",
+    money: 120999000,
+  },
+  {
+    users: 11,
+    date: "11/08/2022",
+    money: 13000000,
+  },
+  {
+    users: 15,
+    date: "11/09/2022",
+    money: 10000000,
+  },
+];
+
+export { demoChartData };
