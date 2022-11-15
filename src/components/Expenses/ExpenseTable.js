2e7a2689cd1e1e793d90d459c6c448d5959e9676
Michelle Santiago <mdcsantiago0@gmail.com>
2022-11-15 13:00:15 +0800

    feat: expense details(total and balance) and budget nav for users only

--- src/components/Expenses/ExpenseTable.js
+++ src/components/Expenses/ExpenseTable.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React,{useState, useEffect} from 'react';
 import { toast } from "react-toastify";
-
+import useProfile from '../hooks/useProfile';
 const ExpenseTable = ({expenses,setExpenses}) => {
+    const {user} = useProfile();
+    const [totalExpense,setTotalExpense]=useState('')
+    
     const showToastMessage = (isSuccess) => {
         if(isSuccess === true) {
             toast.success('Expense Deleted Successfully!', {
@@ -10,40 +12,68 @@ const ExpenseTable = ({expenses,setExpenses}) => {
           });
         } 
     };
+    
+    //map only expenses of current user
+    let userExpenses=[];
+    let total=0;
+    expenses.map((expense)=>{
+        if(expense.email===user.email){
+            userExpenses.push(expense)
+            total+=expense.amount
+        }
+    })
+    useEffect(()=>{
+        setTotalExpense(total)
+        console.log("Total Expense",total)
+    },[expenses])
+    
     // delete expense
-    const deleteExpenses=(amount)=>{
-        const filteredExpenses = expenses.filter((element,index) => {
-            showToastMessage(true);
-            return element.amount !== amount
+    const deleteExpenses=(id)=>{
+        const filteredExpenses=userExpenses.filter((element,index)=>{
+            return element.id !== id
         })
         setExpenses(filteredExpenses);
+        localStorage.setItem('expenses',JSON.stringify(filteredExpenses));
+        showToastMessage(true)
     }
-
+    
     return (
         <>
             <div className='overflow-x-auto pt-5 w-full'>
-                {expenses.length>0&&
+                {totalExpense===0&&
+                     <>
+                        <div>Expense: {total}</div>
+                        <div>Total: {user.balance-total}</div>
+                    </>
+                }
+                {totalExpense!==0&&
+                    <>
+                        <div>Expense: {totalExpense}</div>
+                        <div>Total: {user.balance-totalExpense}</div>
+                    </>
+                }
+                {userExpenses.length>0&&
                     <>
                     <div className=''>
                         <table className='table w-full border text-center'>
                         <thead >
                         <tr>
-                            <th className='bg-slate-800 text-white'></th>
-                            <th className='bg-slate-800 text-white'>Expense</th>
-                            <th className='bg-slate-800 text-white'>Category</th>
-                            <th className='bg-slate-800 text-white'>Amount</th>
-                            <th className='bg-slate-800 text-white'>Delete</th>
+                            <th></th>
+                            <th>Expense</th>
+                            <th>Category</th>
+                            <th>Amount</th>
+                            <th>Delete</th>
                         </tr>
                         </thead>
                         <tbody>
-                            {expenses.map((expense,index)=>(
+                            {userExpenses.map((expense,index)=>(
                             <tr key={index}>
                                 <th>{index+1}</th>
                                 <td>{expense.expenseName}</td>
                                 <td>{expense.category}</td>
                                 <td>{expense.amount}</td>
-                                <td className='delete-btn' onClick={()=>deleteExpenses(expense.amount)}>
-                                <FontAwesomeIcon icon="fa-solid fa-trash"/>
+                                <td onClick={()=>deleteExpenses(expense.id)}>
+                                <i className='fa-solid fa-trash'/>
                                 </td>           
                             </tr>            
                             ))}
@@ -51,13 +81,9 @@ const ExpenseTable = ({expenses,setExpenses}) => {
                         </tbody>
                         </table>
                     </div>
-                    <div className='pt-5 flex justify-center'>
-                    <button className='btn btn-sm border-none bg-primary rounded-xl normal-case'
-                    onClick={()=>setExpenses([])}>Remove All</button> 
-                    </div>
                     </>
                 }
-                {expenses.length < 1 && 
+                {userExpenses.length < 1 && 
                     <div>No expenses are added yet</div>
                 }
             </div>
--- src/pages/Expense.js
+++ src/pages/Expense.js
@@ -3,9 +3,10 @@ import React, {useState, useEffect} from 'react'
 //Import Components
 import AddExpense from '../components/Expenses/CreateExpense';
 import ExpenseTable from '../components/Expenses/ExpenseTable';
-
+import useProfile from '../components/hooks/useProfile';
 // getting the values of local storage
 const getDatafromLS = () => {
+    
     const data = localStorage.getItem('expenses');
     if(data){
       return JSON.parse(data);
@@ -16,20 +17,31 @@ const getDatafromLS = () => {
 }
   
 const Expense = () => {
+  const {user} = useProfile();
+  console.log("expense:",user)
    // main array of objects state || expense state || expense array of objects
-   const [expenses, setExpenses]=useState(getDatafromLS());
-  
-   // saving data to local storage
-   useEffect(()=>{
-     localStorage.setItem('expenses',JSON.stringify(expenses));
-   },[expenses])
+  const [expenses, setExpenses]=useState(getDatafromLS());
 
-    return (
-        <div>
-           <AddExpense expenses={expenses} setExpenses={setExpenses}/>
-           <ExpenseTable expenses={expenses} setExpenses={setExpenses}/> 
-        </div>
-    )
+  console.log("table expense:",user)
+  const expenseHistory= JSON.parse(localStorage.getItem('expenses'))
+
+  // saving data to local storage
+  useEffect(()=>{
+    if (expenseHistory === null) {
+      localStorage.setItem("expenses", JSON.stringify([]));
+    }else{
+      localStorage.setItem('expenses',JSON.stringify(expenses));
+    }
+  },[expenses])
+
+  return (
+      <div>
+          <h2 className="component-header">Budget</h2>
+          <div>Balance: {user.balance}</div>
+          <AddExpense expenses={expenses} setExpenses={setExpenses}/>
+          <ExpenseTable expenses={expenses} setExpenses={setExpenses}/> 
+      </div>
+  )
 }
 
 export default Expense;
