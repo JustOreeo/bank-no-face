571c63e851849e2ebe505a4ab2c77bd9bb4a14eb
lasteeh <lastdavid.lantican@gmail.com>
2022-11-17 07:21:59 +0800

    feat: added confirmation modal to transfers(user and admin) and create account

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Admin from "../pages/Admin";
 import User from "../pages/User";
-import  useProfile from '../components/hooks/useProfile';
+import useProfile from "../components/hooks/useProfile";
 const Dashboard = () => {
-  const {user} = useProfile();
-  console.log(user)
+  const { user } = useProfile();
+  console.log(user);
   return (
     <div className="dashboard">
-      <p className="dash-title">Dashboard</p>
-      {user.role==="Admin"&&<Admin userInfo={user}/>}
-      {user.role==="User"&&<User userInfo={user}/>}
+      <p className="dash-title component-header">Dashboard</p>
+      {user.role === "Admin" && <Admin userInfo={user} />}
+      {user.role === "User" && <User userInfo={user} />}
     </div>
-  )
+  );
 };
 
 export default Dashboard;
